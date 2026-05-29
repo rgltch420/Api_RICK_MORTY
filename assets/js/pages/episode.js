6bc3eee82d65e910c24f8e0d4411d9a0b5a0d949
Silvio Barrios <56333049+silvio1615@users.noreply.github.com>
2026-05-28 20:47:21 -0500

    episodes pages

--- assets/js/pages/episode.js
+++ assets/js/pages/episode.js
@@ -1,20 +1,17 @@
 import { loadHTML } from '../utils/helpers.js';
-import { getEpisode } from '../services/api.js';
-import { episodeCard } from '../components/episodeCard.js';
-
+import { getepisodes } from '../services/api.js';
+import { episodesCard } from '../components/episodesCard.js';
 /**
- * Renderiza episode
+ * Renderiza Episodios
  */
-export async function renderEpisode() {
+export async function renderepisodes() {
     const content = document.getElementById('content');
     content.innerHTML = await loadHTML(
-        './assets/js/views/episode.html'
+        './assets/js/views/episodes.html'
     );
-
-    const container = document.getElementById('episode-container');
-
-    const episodes = await getEpisode ();
+    const container = document.getElementById('episodes-container');
+      const episodes = await getepisodes();
     container.innerHTML = episodes
-        .map(episode=> episodeCard(episode))
+        .map(episode => episodesCard(episode))
         .join('');
 }
