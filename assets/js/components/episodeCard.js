76934fce5ee8595fff96145eaa2fbe5b14a5499b
Silvio Barrios <56333049+silvio1615@users.noreply.github.com>
2026-05-28 20:45:58 -0500

    episodeCard

--- assets/js/components/episodeCard.js
+++ assets/js/components/episodeCard.js
@@ -1,21 +1,29 @@
-export function episodeCard(episode) {
+/**
+ * Episode Card Component
+ */
+
+export function episodesCard(episode) {
+
     return `
-    <div id="episode-container" class="card">
-        <div id="episode-header" class="card-header">
-            <h2 id="episode-name">${episode.name}</h2>
+    <div class="card">
+        
+        <div  class="card-header">
+             <h2 id="episode-name">${episode.name}</h2>
             <span id="episode-code">${episode.episode}</span>
         </div>
 
-        <div id="episode-body" class="card-body">
-            <p id="episode-air-date">
-                <strong>Fecha de emisión:</strong>${episode.air_date}
+        <div  class="card-body">
+
+            <p class="episode-date">
+            <strong>Fecha de emisión:</strong>
+            ${episode.air_date}
             </p>
 
-            <p id="episode-characters">
+            <p class="episode-characters">
             <strong>Cantidad de personajes:</strong>
             <span id="character-count">${episode.characters.length}</span>
             </p>
         </div>
     </div>
-  `;
+    `;
 }
